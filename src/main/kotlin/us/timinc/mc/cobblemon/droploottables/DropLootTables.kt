8876a5f4a2d90fb172ad4e949f464c570f1ed0a2
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-08-20 12:53:40 -0700

    added release trigger for loot drop

--- src/main/kotlin/us/timinc/mc/cobblemon/droploottables/DropLootTables.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/droploottables/DropLootTables.kt
@@ -8,6 +8,7 @@ import net.fabricmc.api.ModInitializer
 import net.minecraft.loot.LootDataType
 import net.minecraft.loot.context.LootContextParameterSet
 import net.minecraft.loot.context.LootContextParameters
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.Vec3d
@@ -62,6 +63,44 @@ object DropLootTables : ModInitializer {
                 )
 
                 event.drops.addAll(list.map(::DynamicItemDropEntry))
+            }
+        }
+        CobblemonEvents.POKEMON_RELEASED_EVENT_POST.subscribe(Priority.LOWEST) { event ->
+            val world = event.player.world
+            if (world.server == null) return@subscribe
+            val pokemon = event.pokemon
+
+            val lootManager = world.server!!.lootManager
+
+            val position = event.player.blockPos
+            val vecPos = Vec3d(position.x.toDouble(), position.y.toDouble(), position.z.toDouble())
+
+            val lootContextParameterSet = LootContextParameterSet(
+                world as ServerWorld, mapOf(
+                    LootContextParameters.ORIGIN to vecPos,
+                    LootContextParameters.KILLER_ENTITY to event.player,
+                    LootConditions.PARAMS.SLAIN_POKEMON to pokemon
+                ), mapOf(), 0F
+            )
+
+            val speciesDropId =
+                modIdentifier("gameplay/releasedrops/species/${pokemon.species.resourceIdentifier.path}")
+            if (lootManager.getIds(LootDataType.LOOT_TABLES).contains(speciesDropId)) {
+                println("Switching to loot table drops for species ${pokemon.species.resourceIdentifier.path}...")
+                val lootTable = lootManager.getLootTable(speciesDropId)
+                val list = lootTable.generateLoot(
+                    lootContextParameterSet
+                )
+                list.forEach { event.player.giveItemStack(it) }
+            }
+
+            val globalDropId = modIdentifier("gameplay/releasedrops/all")
+            if (lootManager.getIds(LootDataType.LOOT_TABLES).contains(globalDropId)) {
+                val lootTable = lootManager.getLootTable((globalDropId))
+                val list = lootTable.generateLoot(
+                    lootContextParameterSet
+                )
+                list.forEach { event.player.giveItemStack(it) }
             }
         }
     }
