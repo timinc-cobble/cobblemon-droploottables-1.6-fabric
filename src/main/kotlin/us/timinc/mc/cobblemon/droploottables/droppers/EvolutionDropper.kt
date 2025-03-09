c67a8bc5fc5393f88331f3c13366ba13d4017ae4
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-03-09 14:09:08 -0700

    WIP

--- src/main/kotlin/us/timinc/mc/cobblemon/droploottables/droppers/EvolutionDropper.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/droploottables/droppers/EvolutionDropper.kt
@@ -3,26 +3,21 @@ package us.timinc.mc.cobblemon.droploottables.droppers
 import com.cobblemon.mod.common.api.Priority
 import com.cobblemon.mod.common.api.events.CobblemonEvents
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.level.storage.loot.LootParams
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams
 import us.timinc.mc.cobblemon.droploottables.api.droppers.AbstractFormDropper
 import us.timinc.mc.cobblemon.droploottables.api.droppers.FormDropContext
 import us.timinc.mc.cobblemon.droploottables.lootconditions.LootConditions
 
-object KoDropper : AbstractFormDropper("ko") {
+object EvolutionDropper : AbstractFormDropper("evolve") {
     override fun load() {
-        CobblemonEvents.POKEMON_FAINTED.subscribe(Priority.LOWEST) { event ->
+        CobblemonEvents.EVOLUTION_COMPLETE.subscribe(Priority.LOWEST) { event ->
             val dropperEntity = event.pokemon.entity
             val player = event.pokemon.getOwnerPlayer() ?: return@subscribe
             val positionalEntity = dropperEntity ?: player
             val level = positionalEntity.level()
             if (level !is ServerLevel) return@subscribe
             val position = positionalEntity.position()
-            val wasInBattle = dropperEntity?.isBattling ?: false
-            val attacker = dropperEntity?.lastAttacker
-            val directAttackingEntity = dropperEntity?.lastDamageSource
-            val attackingPlayer = if (attacker is ServerPlayer) attacker else null
 
             val lootParams = LootParams(
                 level,
@@ -30,10 +25,7 @@ object KoDropper : AbstractFormDropper("ko") {
                     LootContextParams.ORIGIN to position,
                     LootContextParams.THIS_ENTITY to dropperEntity,
                     LootConditions.PARAMS.POKEMON_DETAILS to event.pokemon,
-                    LootConditions.PARAMS.WAS_IN_BATTLE to wasInBattle,
-                    LootContextParams.ATTACKING_ENTITY to attacker,
-                    LootContextParams.DIRECT_ATTACKING_ENTITY to directAttackingEntity,
-                    LootContextParams.LAST_DAMAGE_PLAYER to attackingPlayer
+                    LootConditions.PARAMS.EVOLUTION to event.evolution
                 ),
                 mapOf(),
                 player.luck
