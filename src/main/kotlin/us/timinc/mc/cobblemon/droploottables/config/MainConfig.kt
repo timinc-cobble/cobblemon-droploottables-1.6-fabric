177221bdd7efb18ebde3f9fbe02a288ba7a831e9
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-03-11 14:39:29 -0700

    added a periodic dropper
    * complete with granular PokemonProperties override for the timer
    * moved KO dropper wild check to event
    * added a PokemonTicked Event, WildPokemonTickedEvent, and WildPokemonFainted event

--- src/main/kotlin/us/timinc/mc/cobblemon/droploottables/config/MainConfig.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/droploottables/config/MainConfig.kt
@@ -1,6 +1,14 @@
 package us.timinc.mc.cobblemon.droploottables.config
 
+import com.cobblemon.mod.common.api.pokemon.PokemonProperties
+import com.cobblemon.mod.common.pokemon.Pokemon
+import us.timinc.mc.cobblemon.droploottables.toIntRange
+
 class MainConfig {
     var debug: Boolean = false
     var useCobblemonDropsIfOverrideNotPresent = true
+    var granularDropPeriods: Map<String, String> = mapOf()
+
+    fun getGranularDropPeriodFor(pokemon: Pokemon): IntRange? =
+        granularDropPeriods.entries.find { (k) -> PokemonProperties.parse(k).matches(pokemon) }?.value?.let(::toIntRange)
 }
