b3234e1e4d346feef581f1321d88363cd57c0108
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-03-14 12:23:03 -0700

    hunt down the entity if not given for resurrection

--- src/main/kotlin/us/timinc/mc/cobblemon/droploottables/droppers/FossilRevivedDropper.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/droploottables/droppers/FossilRevivedDropper.kt
@@ -1,7 +1,12 @@
 package us.timinc.mc.cobblemon.droploottables.droppers
 
 import com.cobblemon.mod.common.api.events.CobblemonEvents
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import com.cobblemon.mod.common.pokemon.Pokemon
+import com.cobblemon.mod.common.pokemon.activestate.SentOutState
+import com.cobblemon.mod.common.util.server
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.level.storage.loot.LootParams
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams
 import us.timinc.mc.cobblemon.droploottables.api.droppers.AbstractFormDropper
@@ -11,12 +16,13 @@ import us.timinc.mc.cobblemon.droploottables.lootconditions.LootConditions
 object FossilRevivedDropper : AbstractFormDropper("resurrect") {
     override fun load() {
         CobblemonEvents.FOSSIL_REVIVED.subscribe { event ->
-            val level = event.player?.level() ?: return@subscribe
-            if (level !is ServerLevel) return@subscribe
-            val player = event.player ?: return@subscribe
-            val position = player.position()
             val pokemon = event.pokemon
-            val pokemonEntity = pokemon.entity
+            val player = event.player
+            val pokemonEntity = pokemon.entity ?: findPokemonEntity(pokemon)
+            val positionalEntity = player ?: pokemonEntity ?: return@subscribe
+            val level = positionalEntity.level()
+            if (level !is ServerLevel) return@subscribe
+            val position = positionalEntity.position()
             val form = pokemon.form
 
             val lootParams = LootParams(
@@ -28,7 +34,7 @@ object FossilRevivedDropper : AbstractFormDropper("resurrect") {
                     LootConditions.PARAMS.RELEVANT_PLAYER to player,
                 ),
                 mapOf(),
-                player.luck
+                player?.luck ?: 0F
             )
             val context = FormDropContext(form)
             val drops = getDrops(
@@ -36,7 +42,17 @@ object FossilRevivedDropper : AbstractFormDropper("resurrect") {
                 context
             )
 
-            giveDropsToPlayer(drops, player)
+            player?.let { giveDropsToPlayer(drops, it) } ?: drops.forEach { drop ->
+                level.addFreshEntity(ItemEntity(level, pokemonEntity!!.x, pokemonEntity.y, pokemonEntity.z, drop))
+            }
         }
+    }
+
+    private fun findPokemonEntity(pokemon: Pokemon): PokemonEntity? {
+        val server = server() ?: return null
+        server.allLevels.forEach { level ->
+            return level.allEntities.firstOrNull { it is PokemonEntity && it.pokemon == pokemon } as? PokemonEntity
+        }
+        return null
     }
 }
